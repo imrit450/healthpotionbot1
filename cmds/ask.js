c2e3b984f2efa6ce482c4655ff4b2320fb54760b
Ismail Imrit <imrit450@gmail.com>
2018-07-09 17:28:59 +0400

    sss

--- cmds/ask.js
+++ cmds/ask.js
@@ -2,20 +2,20 @@ const Discord = module.require("discord.js");
 
 module.exports.run = async (bot,message,args) => {
     let answers = [
-        "Yes <:pikaglance:460475587904012327>",
+        "Yes",
         "No",
         "Maybe <:pikaglance:460475587904012327>",
-        "ehhhh Yes..maybe? 🤔",
+        "ehhhh Yes..maybe? <:whytho:378183787600805889>",
         "ehhhh No..maybe? <:huh:460482102719283210>",
-        "ummm what? why? 😐",
-        "Idk m8, better ask Ismail 🙄",
-        "Ofc Darling 😘",
+        "ummm what? why? <:wait:463468116014530571>",
+        "Idk m8, better ask Ismail <:pepehigh:463046642140971009>",
+        "Ofc Darling <:GWcmeisterPeepoLove:403295311189245952>",
         
     ];
     
-    if ((message.author.id === '164643701342076928') && (message.content.endsWith("??"))) {(message.channel.sendMessage ("Ofc Darling 😘 "));}
+    if ((message.author.id === '164643701342076928') && (message.content.endsWith("??"))) {(message.channel.sendMessage ("Ofc Darling <:GWcmeisterPeepoLove:403295311189245952>"));}
         else
-            if ((message.content.endsWith(".?")) && (message.author.id === '164643701342076928')) {message.channel.sendMessage("Yes <:pikaglance:460475587904012327>");}
+            if ((message.content.endsWith(".?")) && (message.author.id === '164643701342076928')) {message.channel.sendMessage("Yes");}
                 else
                      if (args[1]) message.channel.sendMessage(answers[Math.floor(Math.random(3342432341) * answers.length)]);
                         else message.channel.sendMessage("Ask me a question...");
